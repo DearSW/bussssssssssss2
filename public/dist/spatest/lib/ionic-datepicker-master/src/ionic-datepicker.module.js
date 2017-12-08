25b352ea3e9e90e9b110345462b3bf8793a3d16d
DearSW <826836334@qq.com>
2017-12-08 16:07:09 +0800

    xxxxxxxxxxxxxxxxxxxxxx

---
+++ public/dist/spatest/lib/ionic-datepicker-master/src/ionic-datepicker.module.js
@@ -0,0 +1,1 @@
+angular.module("ionic-datepicker",["ionic","ionic-datepicker.service","ionic-datepicker.provider","ionic-datepicker.templates"]);
--- public/fis-conf.js
+++ public/fis-conf.js
@@ -3,26 +3,26 @@ fis.match('::packager', {
    spriter: fis.plugin('csssprites')
  });
 //是否开启文件指纹
- fis.match('*', {
+ fis.match('**', {
    useHash: false
  });
 //压缩js，节省代码体积
- fis.match('*.js', {
+ fis.match('**.js', {
    optimizer: fis.plugin('uglify-js',{
        mangle:false,//不开启变量混淆
    })
  });
 //压缩css
- fis.match('*.css', {
+ fis.match('**.css', {
    useSprite: true,
    optimizer: fis.plugin('clean-css')
  });
 //压缩png图片
- fis.match('*.png', {
+ fis.match('**.png', {
    optimizer: fis.plugin('png-compressor')
  });
 //压缩html代码
- fis.match('*.html', {
+ fis.match('**.html', {
      optimizer: fis.plugin('html-compress')
  })
 //打包页面引用的文件
