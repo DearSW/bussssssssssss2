433e2325756686a61cbd4b2eb95d2ee2f58a74ad
DearSW <826836334@qq.com>
2017-12-11 16:36:34 +0800

    城市选择添加触摸事件

--- public/spatest/a-bugubus/directive.js
+++ public/spatest/a-bugubus/directive.js
@@ -486,10 +486,10 @@ app.directive('scrollpin', function() {
 
                 if(c >= 5) {
 
-                    c.style.cssText += 'box-shadow: 0 0 5px 3px #ddd;';
+                    angular.element(element[0]).style.cssText += 'box-shadow: 0 0 5px 3px #ddd;';
 
                 } else {
-                    c.style.cssText += 'box-shadow: none;';
+                    angular.element(element[0]).style.cssText += 'box-shadow: none;';
 
                 }
             }
